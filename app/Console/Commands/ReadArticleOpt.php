5bcec600995ff17057e0b1248ee6857618a18a91
honourfuture <honourfuture@163.com>
2023-07-21 00:05:44 +0800

    feat(Controller): 修复opt文件

--- app/Console/Commands/ReadArticleOpt.php
+++ app/Console/Commands/ReadArticleOpt.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Console\Commands;
 use App\Models\Article;
+use App\Models\Chapter;
 use Illuminate\Support\Facades\Storage;
 
 use Illuminate\Console\Command;
@@ -12,7 +13,7 @@ class ReadArticleOpt extends Command
      *
      * @var string
      */
-    protected $signature = 'read:opt {--article_id=}';
+    protected $signature = 'read:opt';
 
     /**
      * The console command description.
@@ -38,6 +39,8 @@ class ReadArticleOpt extends Command
      */
     public function handle()
     {
+        logger('article id loss', [111]);die;
+
         $storage = Storage::disk('article');
 
         $files =  $storage->directories('/');
@@ -46,18 +49,79 @@ class ReadArticleOpt extends Command
             $article_ids = $storage->directories("/{$index}/");
             foreach ($article_ids as $id){
                 $article_id = explode('/', $id)[1];
+                if($article_id == 0){
+                    continue;
+                }
                 $index_opf = $storage->get("/{$id}/index.opf");
                 $strXml = iconv('gbk', 'utf-8//IGNORE', $index_opf);
                 $strXml = str_replace('ISO-8859-1', 'UTF-8', $strXml);
                 @$objXml = simplexml_load_string($strXml);
                 $book = json_decode(json_encode($objXml), true);
-                print_r($book);die;
+
+                $bookName = $book['metadata']['dc-metadata']['dc:Title'];
+                $author = $book['metadata']['dc-metadata']['dc:Creator'];
+
                 $article = Article::where('articleid', $article_id)->with(
                     'chapters'
-                )->first()->toArray();
+                )->first();
+
+                if(in_array($article_id, [101])){
+                    continue;
+                }
 
                 if(!$article){
+                    logger('article id loss', [$article_id]);
+                    echo('article id loss' . $article_id."\n");
+                    continue;
+                }
 
+                $article = $article->toArray();
+
+                $optChapters = $book['manifest']['item'];
+                $articleChapters = $article['chapters'];
+
+                if(count($optChapters) <> count($articleChapters)){
+                    $chapterIds = array_column($articleChapters, 'chapterid');
+
+                    logger('chapters loss', [
+                        'article_id' => $article_id,
+                        'book_name' => $bookName,
+                        'author' => $author,
+                    ]);
+
+                    $order = 0;
+                    foreach ($optChapters as $chapter){
+                        $order++;
+                        $chapterId = str_replace('.txt', '', $chapter['@attributes']['href']);
+                        if(!in_array($chapterId, $chapterIds)){
+
+                            $chapter_file_path = "{$index}/{$article_id}/{$chapterId}.txt";
+                            $size = $storage->size($chapter_file_path);
+                            $insert = [
+                                'chapterid' => $chapterId,
+                                'articleid' => $article_id,
+                                'articlename' => $bookName,
+                                'posterid' => 1,
+                                'poster' => 'admin',
+                                'postdate' => time(),
+                                'lastupdate' => 1602259200,
+                                'chaptername' => $chapter['@attributes']['id'],
+                                'chapterorder' => $order,
+                                'size' => $size,
+
+                            ];
+
+                            logger('chapters loss', [
+                                'article_id' => $article_id,
+                                'book_name' => $bookName,
+                                'author' => $author,
+                                'opt' => $chapter,
+                                'insert' => $insert,
+                            ]);
+
+                            Chapter::insert($insert);
+                        }
+                    }
                 }
             }
         }
