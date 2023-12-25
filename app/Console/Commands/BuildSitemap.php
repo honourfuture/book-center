0de824b2ad6bacb9084aaa9fd8f481de6a8f32b4
honourfuture <honourfuture@163.com>
2023-12-25 14:40:07 +0800

    feat(Console): build sitemap修改为301文件

--- app/Console/Commands/BuildSitemap.php
+++ app/Console/Commands/BuildSitemap.php
@@ -2,8 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Article;
+use App\Models\Chapter;
 use App\Services\SitemapService;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class BuildSitemap extends Command
 {
@@ -38,9 +41,65 @@ class BuildSitemap extends Command
      */
     public function handle()
     {
-        /** @var SitemapService $sitemapService */
-        $sitemapService = app('SitemapService');
-        $sitemapService->build_update_sitemap();
+        $i = 0;
+        $lines = [
+            'pc' => [],
+            'wap' => [],
+            'top' => []
+        ];
+        $page = 1;
+
+        Article::select(['articleid'])->orderby('articleid', 'asc')->chunk(500, function ($articles) use (&$i, &$lines, &$page) {
+            $urls = [
+                'pc' => [
+                    'old' => 'https://www.tieshuw.com',
+                    'new' => 'https://www.lexinren.top'
+                ],
+                'wap' => [
+                    'old' => 'https://m.tieshuw.com',
+                    'new' => 'https://m.lexinren.top'
+                ],
+                'top' => [
+                    'old' => 'https://tieshuw.com',
+                    'new' => 'https://lexinren.top'
+                ],
+            ];
+
+            foreach ($articles as $article){
+                $i++;
+                $index = intval($article->articleid / 1000);
+                $href = "/{$index}_{$article->articleid}/";
+                $new_article_id = $article->articleid + 13;
+                $new_href = "/biquge_{$new_article_id}/";
+
+
+                foreach ($urls as $key => $url){
+                    $static_url = $url['old'].$href.' '.$url['new'].$new_href."\n";
+                    $lines[$key][] = $static_url;
+                }
+
+                if($i == 2000){
+                    foreach ($lines as $key => $line){
+                        Storage::put("/{$key}_{$page}.txt", $line);
+                    }
+                    $lines = [
+                        'pc' => [],
+                        'wap' => [],
+                        'top' => []
+                    ];
+                    $page++;
+                    echo $page;
+
+                    $i=0;
+                }
+            }
+        });
+
+
+//        Chapter::select(['articleid', 'chapterid'])->chunk(500, function ($articles) use ($i) {
+//
+//
+//        });
     }
 
 }
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chapter;
+use App\Models\Domain;
 use App\Models\ErrorChapter;
 use App\Models\HandArticle;
 use App\Services\ExcellentArticleService;
@@ -20,22 +21,52 @@ class TestController extends Controller
 {
     public function test()
     {
+        $key = '3d5007f6cc4763d314194b114f7d73a1';
+        $domains = [
+            'hncsr.org','fhzwxs.com','ahjinli.net','juzhihai.info','rongdabpfhw.com','china-tense.cn','fennessy.cc','999hebao.vip','xiaoyuantang.org','xytsw.net','namasidan.com','545667.com','nveshua.com','wenhuazhai.com','fairstery.com','shzysb.com','huizhishuge.com','sangushuwu.com','damanet.cn','sdzi.cc','881291.org','jxwx365.com','yikuao.com','17dushu.top','lexinren.top','shibawai.top','678xs.cn','49xswz.cn','wqwhw.com.cn','kayege.vip','shuhaha.net','xiaoshuo92.com','yuchuanshuwu.com','didashuwu.com','4qxs.com','jdxs5200.com','51kanshuu.com','my188ty.com','rxxsww.com','wfbsr.com','niuttt.com','didatxtt.com','hkslg520.com','1stbg.com','aishuya.com','qdianx.com','1jingdian.com','irss.me','inmask.me','6b6b.me','haitang123.live','laishushu.la','shuke.la','lawen2.org','tjdhlt.com.cn','neixiong.net','hfrxs.com','qidiancom.com','yzltxs.com','zcldi.cn','qydc10z.com','neixiong8.com','shubao.in','txtnovel.cc','8xxs.org','81byby.org','d9zww.net','692211.net','miaojiangdaoshi.net','uukxs.com','afxfs.com','chenhuiwx.com','bengou.com.tw','kujiang.co','kanshu.la','69zw.la','chinasinyi.com','aixiaoshu.top','jinkouqiche.vip','remenxiaoshuo.net','nvecheng.com','xiaoshuo338.com','yunxibook.com','novelser.com','hebao.la','datuba.com','nbw.la','12zw.la','soloshu.co','zwdu.tw','xiaoshuoli.tw','shubao8.tw','shubao5.tw','shengyan.tw','sanjiangge.tw','lewenxiaoshuo.tw','heiyange.tw','hanxiang.tw','baoliny.tw','88dus.tw','2kxs.tw','shuhai.me','zhoumoxiaoshuo.com','xiaobaixinwen.com','kuaiyan5.com','humaicai.com','dh379.com','biyi5.com','biqupc.com','33dus.com','lawen2.org','91kanshu.vip','tdsmeter.cc','babai.cc','tingxiaoshuo.com.cn','paofuw.cn','lsisi.cn','jyanyu.cn','fuyaot.cn','dleyou.cn','dbizhi.cn','angexs.cn','ahouge.cn','adianya.cn','qingnanxs.top','shapolang.org','mmbook.tw','kenshu.tw','cyimei.cn','87dus.com','4qxs.com','rxxsww.com','guishenwushuang.com','rpxonline.cn','wqwhw.com.cn','881291.org','8xxs.org','d9zww.net','neixiong8.net','hkslg520.com','51kanshuu.com','chenhuiwx.com','niuttt.com','jdxs5200.com','rzt485.com','yzltxs.com','n527.com','didatxtt.com','bengou.com.tw','kujiang.co','oread.link','69zw.la','laishushu.la','23hh.la','ymyxsw.com','bf95.net','hfrxs.com','miaojiangdaoshi.net','81byby.org','qidiancom.com','ranhouaiqingsuiyueran.com','ltxs123.com','shubaoer.com','sgshy.com','neixiong8.com','biquge.la','kanshu5.la'
+        ];
+
+        $domains = array_chunk($domains, 3);
+        foreach ($domains as $domain){
+            $domain_urls = implode('|', $domain);
+            $client = new Client([
+                'base_uri' => "https://apistore.aizhan.com/site/hisinfos/{$key}?domains={$domain_urls}&type=0",
+                'timeout' => 3.0
+            ]);
+
+            $response = $client->request('GET');
+            $result = $response->getBody()->getContents();
+
+//            $result = '{"code":200000,"status":"success","data":{"success":[{"domain":"hncsr.org","pc_br":0,"pc_sum":0,"pc_br_max":1,"pc_nums_max":11,"pc_max_time":"2017-10-27","pc_br_min":0,"pc_nums_min":2,"pc_min_time":"2017-08-17","m_br":0,"m_sum":0,"m_br_max":0,"m_nums_max":17,"m_max_time":"2022-11-26","m_br_min":0,"m_nums_min":1,"m_min_time":"2017-09-13"},{"domain":"fhzwxs.com","pc_br":0,"pc_sum":1,"pc_br_max":1,"pc_nums_max":50,"pc_max_time":"2022-12-15","pc_br_min":0,"pc_nums_min":15,"pc_min_time":"2022-11-02","m_br":0,"m_sum":0,"m_br_max":2,"m_nums_max":16,"m_max_time":"2022-11-08","m_br_min":0,"m_nums_min":9,"m_min_time":"2022-11-02"}],"count":2,"failed":[]},"msg":"\u8bf7\u6c42\u6210\u529f"}';
+            $result = json_decode($result, true);
+
+            if($result['code'] <> 200000){
+                echo $domain_urls."\n";
+                print_r($result);
+                continue;
+            }
+
+            $result = $result['data']['success'];
+            Domain::insert($result);
+        }
+        die;
+
         Artisan::call("collect:article", []);
         die;
 
         /** @var HttpProxyService $httpProxyService */
         $httpProxyService = app("HttpProxyService");
 
-        for ($i = 7; $i< 488; $i++){
+        for ($i = 7; $i < 488; $i++) {
             $proxy = $httpProxyService->proxy();
             $proxyAuth = base64_encode('DS2ZMP8Q' . ":" . '5F7CFBFE8427');
             $url = "https://top.aizhan.com/top/t3-15/p{$i}.html";
 
             try {
                 $rt = QueryList::get($url, [], [
-                    'proxy' => "http://".$proxy,
+                    'proxy' => "http://" . $proxy,
                     'headers' => [
-                        'User-Agent' =>  $httpProxyService->user_agent(),
+                        'User-Agent' => $httpProxyService->user_agent(),
                         'Accept-Encoding' => 'gzip, deflate, br',
                         "Proxy-Authorization" => "Basic " . $proxyAuth
                     ],
@@ -47,14 +78,15 @@ class TestController extends Controller
                 ])
                     ->range('.text>h2')->query()->getData();
 
-                print_r($rt);die;
-                foreach ($rt as $value){
-                    $info = [ $value['name'].'-'.$value['domain'] ];
+                print_r($rt);
+                die;
+                foreach ($rt as $value) {
+                    $info = [$value['name'] . '-' . $value['domain']];
                     logger('success', $info);
                 }
                 sleep(3);
                 die;
-            }catch (\Exception $exception){
+            } catch (\Exception $exception) {
                 sleep(1);
                 print_r($exception->getMessage());
                 logger('error', [$url]);
