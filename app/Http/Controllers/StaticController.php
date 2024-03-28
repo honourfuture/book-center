0567bd22de98d1c3fbee5453bd6d40c99bfe7c31
honourfuture <honourfuture@163.com>
2024-03-28 14:49:35 +0800

    feat(Controller/View): 增加新书

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Enums\QueueNameEnum;
 use App\Jobs\AutoArticleAllJob;
 use App\Jobs\CrontabUpdateArticleJob;
+use App\Models\Article;
 use App\Models\BookUpdateArticle;
 use App\Models\SourceArticle;
 use Carbon\Carbon;
@@ -55,13 +56,54 @@ class StaticController extends Controller
     {
         $site = $request->get('site', 'mayi');
         $type = $request->get('type', 2);
-        $update_articles = BookUpdateArticle::where('type', $type)
+
+        $source_articles = SourceArticle::where('local_article_id', 0)
+            ->where('source', $site)
+            ->orderBy('id', 'desc')
+            ->limit(100)
+            ->get()->keyBy(function ($article) {
+                return md5($article->article_name . '-' . $article->author);
+            });
+
+        $authors = $source_articles->pluck('author');
+        $article_names = $source_articles->pluck('article_name');
+
+        $articles = Article::select(['articleid', 'articlename', 'author'])->whereIn('articlename', $article_names)
+            ->whereIn('author', $authors)->get()->keyBy(function ($article) {
+                return md5($article->articlename . '-' . $article->author);
+            })->toArray();
+
+
+        $add_articles = [];
+        foreach ($source_articles as $key => $source_article) {
+            if (isset($articles[$key])) {
+                SourceArticle::where('id', $source_article->id)
+                    ->update(['local_article_id' => $articles[$key]['articleid']]);
+                continue;
+            }
+
+            $source_article_id = $source_article['article_id'];
+            $index = intval($source_article_id / 1000);
+            $add_articles[] = [
+                'article_url' => "https://www.mayiwsk.com/{$index}_{$source_article_id}/index.html",
+                'article_name' => $source_article['article_name'],
+                'author' => $source_article['author'],
+                'last_chapter' => ''
+            ];
+
+        }
+
+        $update_articles = BookUpdateArticle::select(['article_name', 'article_url', 'author', 'last_chapter'])
+            ->where('type', $type)
             ->where('site', $site)
             ->where('local_article_id', 0)
             ->limit(100)
             ->get();
 
         $ids = $update_articles->pluck('id');
+        $update_articles = $update_articles->toArray();
+
+        $update_articles = array_merge($update_articles, $add_articles);
 
         BookUpdateArticle::whereIn('id', $ids)->delete();
         return view('static/mayi', ['update_articles' => $update_articles]);
--- resources/views/static/mayi.blade.php
+++ resources/views/static/mayi.blade.php
@@ -4,7 +4,7 @@
         <h2>最近更新小说列表</h2>
         <ul>
             @foreach($update_articles as $update_article)
-            <li><span class="s1"></span><span class="s2"><a href="{{$update_article['article_url']}}" target="_blank">{{$update_article['article_name']}}</a></span><span class="s3"><a href="/103_103013/48082515.html" target="_blank">{{ $update_article['last_update'] }}</a></span><span class="s4">{{ $update_article['author'] }}</span><span class="s5">10-29</span></li>
+            <li><span class="s1"></span><span class="s2"><a href="{{$update_article['article_url']}}" target="_blank">{{$update_article['article_name']}}</a></span><span class="s3"><a href="/103_103013/48082515.html" target="_blank">{{ $update_article['last_chapter'] }}</a></span><span class="s4">{{ $update_article['author'] }}</span><span class="s5">10-29</span></li>
             @endforeach
         </ul>
     </div>
