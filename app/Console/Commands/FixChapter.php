1d56271f845213b3891b5d7a97fd23ba694e44ce
honourfuture <honourfuture@163.com>
2024-05-07 00:11:00 +0800

    feat(Command): tt修改上限为500

--- app/Console/Commands/FixChapter.php
+++ app/Console/Commands/FixChapter.php
@@ -62,6 +62,9 @@ class FixChapter extends Command
         if (!$site) {
             $site = 'mayi';
         }
+        if($site == 'tt'){
+            $limit = 500;
+        }
         /** @var SpiderService $spiderService */
         $this->spiderService = app('SpiderService', [
             'site' => $site,
