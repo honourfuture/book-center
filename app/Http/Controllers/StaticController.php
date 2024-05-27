df03c70e4d7239bba4bfac3bb52be7297c802a4b
honourfuture <honourfuture@163.com>
2024-05-27 14:06:40 +0800

    feat(Controller): 读取后删除

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -37,8 +37,13 @@ class StaticController extends Controller
     {
         $site = $request->get('site', 'mayi');
         $type = $request->get('type', 1);
-        $update_articles = BookUpdateArticle::where('type', $type)->where('site', $site)->limit(200)->get();
+        $update_articles = BookUpdateArticle::where('type', $type)->where('is_push_job', 1)
+            ->where('site', $site)
+            ->orderBy('id', 'desc')
+            ->limit(200)
+            ->get();
         $ids = $update_articles->pluck('id');
+        BookUpdateArticle::whereIn('id', $ids)->delete();
 
         return view('static/mayi', ['update_articles' => $update_articles]);
     }
